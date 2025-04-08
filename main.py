02a71eff927607e1db8842ac62218cfb61a4bbdd
Shaheer <shahrs55555@gmail.com>
2025-04-08 14:02:37 +0500

    added venv

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import numpy as np
+
+x_train = np.array(10) # Input (Must find out what inputs to generate)
+y_train = np.array(10) # Output (Can be taken from the rainbow table)
+
+print(x_train)
